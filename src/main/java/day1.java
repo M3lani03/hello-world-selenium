9ed57d12c05aa5cdded9ec2004c4edb3fa36bf4d
Melanie Vasseur <vasseurmelanie.pro@gmail.com>
2021-04-19 16:14:13 +0200

    modif

--- src/main/java/day1.java
+++ src/main/java/day1.java
@@ -24,7 +24,7 @@ public class day1 {
         caps.setPlatform(Platform.LINUX);
         driver = new RemoteWebDriver(new
                 URL("http://seleniumhub:4444/wd/hub"), caps);
-        driver.get("http://54.39.94.235:8090/webapp");
+        driver.get("http://66.70.247.82:8090/webapp");
     }
     @Test
     public void searchString() {
